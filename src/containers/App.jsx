63f7aaeac171522a083c49dfaf825e2649643a8f
Luis Chávez <luischdu@gmail.com>
2021-05-21 22:32:28 -0500

    adding cosillas

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Buscador from '../components/Buscador';
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
+import Title from '../components/Title';
 import ListMealsMainPage from './ListMealsMainPage';
-
 
 const App = () => {
   return (
     <Main>
-      <Navbar/>
-      <Buscador/>
-      <ListMealsMainPage/>
+      <Navbar />
+      <Title />
+      <Buscador />
+      <ListMealsMainPage />
     </Main>
   );
 };
