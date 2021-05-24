2bf8ae36062af30fbe36f943ee37b89a3c287e34
Jhosep Ropero <roma.jhosep@gmail.com>
2021-05-24 02:32:02 -0500

    Solve Bugs

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const stripe = new Stripe("sk_test_51IuQhODxWwRTK4xqrhuMON11XQJDsXmYgiUIxocDCo8HZ5j7SoWOsgupVYjEcvnwK59k6EBkbnFAZnHgtKvBeku200QFtNykXP")
 
 app.use(cors({ origin: 'https://60ab2bd0f1a98400090f27be--vigilant-bose-b9ad67.netlify.app' }))
-
+//https://60ab2bd0f1a98400090f27be--vigilant-bose-b9ad67.netlify.app
 app.use(express.json())
 
 app.post('/api/checkout', async (req, res) => {
